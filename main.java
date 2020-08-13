15af7607c036219263de57cf8046e19cd1e74da4
FardinHaque60 <67034724+FardinHaque60@users.noreply.github.com>
2020-08-13 14:22:15 -0700

    Update main.java

--- main.java
+++ main.java
@@ -9,8 +9,8 @@ public class Main
     System.out.println("I like to run in my free time");
     System.out.println("My favorite subject in school is math or science");
     System.out.println("I would like to go to UCLA");
-    System.out.println("My passions reside in mechnical and software engineering");
-    System.out.println("Some of my favroite foods are french fries and chicken tenders");
+    System.out.println("My passions reside in mechanical and software engineering");
+    System.out.println("Some of my favorite foods are french fries and chicken tenders");
 
 	}
 }
